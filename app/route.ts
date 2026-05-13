79299b6d2ec526877cec6cd28ec85e8c812db583
samuel Hebron <hebronsamuel252@gmail.com>
2026-05-13 10:43:32 +0300

    fix: redirect root / to /login

--- app/route.ts
+++ app/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  return NextResponse.json({ message: "Hello world!" });
+  return NextResponse.redirect(new URL("/login", "https://fyp-backend-pi-one.vercel.app"));
 }
